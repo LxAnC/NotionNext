5f3a274423e330a23aa357472639c32797a10180
LxAnC <76821521+LxAnC@users.noreply.github.com>
2024-06-06 13:26:40 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: '算法相关',
   HEO_HERO_TITLE_4: '实用教程',
   HEO_HERO_TITLE_5: '资源专栏',
-  HEO_HERO_TITLE_LINK: 'lxanc.top',
+  HEO_HERO_TITLE_LINK: 'category/learning/',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/jingxuan' },
